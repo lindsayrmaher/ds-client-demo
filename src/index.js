3913145e061d600112bae99c57a417305bf6e138
Lindsay Maher <lindsaymah3r@gmail.com>
2019-07-07 19:08:06 -0400

    adds appolo client setup & dependencies

--- src/index.js
+++ src/index.js
@@ -5,10 +5,26 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
