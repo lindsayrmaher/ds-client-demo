0918acfb8654d5d2f7e031db72cb4a4fd29bd46a
Lindsay Maher <lindsaymah3r@gmail.com>
2019-07-07 19:51:25 -0400

    slight update to CSS

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ListItem = (props) => {
     const item = props.item
     return (
-        <div>
+        <div className="flex flex-column mt3">
             Title: {item.title}
             Author: {item.author ? item.author : 'no author available'}
             Date Published: {item.publishedAt}
